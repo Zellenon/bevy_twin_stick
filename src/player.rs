f0e60b4ec6e583f30630c244a077084ca9a209ac
Zellenon <zellenon@gmail.com>
2023-10-18 16:33:04 -0500

    moved camera functions

--- src/player.rs
+++ src/player.rs
@@ -2,21 +2,18 @@ use crate::utils::*;
 use crate::weapons::{FireWeaponEvent, Weapon, WeaponFireMode};
 
 use bevy::prelude::{
-    default, App, Camera2dBundle, Children, Commands, Component, Entity, EventWriter, Input,
-    IntoSystemConfigs, MouseButton, Name, Plugin, Query, Res, Resource, Startup, Update, Vec2,
-    With,
+    App, Camera2dBundle, Children, Commands, Component, Entity, EventWriter, Input,
+    IntoSystemConfigs, MouseButton, Name, Plugin, Query, Res, Resource, Startup, Update, With,
 };
 use bevy::window::Window;
 use bevy_mod_transform2d::prelude::Spatial2dBundle;
 use bevy_mod_transform2d::transform2d::Transform2d;
-use bevy_prototype_lyon::prelude::{GeometryBuilder, ShapeBundle};
-use bevy_prototype_lyon::shapes;
 
 #[derive(Component, Resource)]
 pub struct MainCamera(pub Entity);
 
 #[derive(Component)]
-pub struct ArenaCamera;
+pub struct TwinStickCamera;
 
 #[derive(Component)]
 pub struct CursorTracker;
@@ -36,36 +33,26 @@ impl Plugin for PlayerPlugin {
             Update,
             (
                 update_cursor_tracker,
-                (fire_player_weapons, camera_movement).run_if(player_exists),
+                fire_player_weapons.run_if(player_exists),
             ),
         );
-        //     .add_system(update_cursor_tracker);
-        // app.add_system(fire_player_weapons.run_if(player_exists))
-        //     .add_system(camera_movement.run_if(player_exists));
     }
 }
 
 pub fn player_setup(mut commands: Commands) {
     let camera_entity = commands
         .spawn(Camera2dBundle::default())
-        .insert(Name::new("Camera"))
+        .insert(Name::new("Twin-Stick Player Camera"))
         .insert(Transform2d {
             z_translation: 100.,
             ..Default::default()
         })
-        .insert(ArenaCamera)
+        .insert(TwinStickCamera)
         .id();
     commands.insert_resource(MainCamera(camera_entity));
 
     let cursor_entity = commands
         .spawn(Spatial2dBundle::default())
-        .insert(ShapeBundle {
-            path: GeometryBuilder::build_as(&shapes::Rectangle {
-                extents: Vec2::new(20., 20.),
-                origin: bevy_prototype_lyon::shapes::RectangleOrigin::Center,
-            }),
-            ..default()
-        })
         .insert(Name::new("Cursor"))
         .insert(CursorTracker)
         .id();
@@ -92,7 +79,7 @@ pub fn player_exists(players: Query<(), With<Player>>) -> bool {
     players.iter().count() > 0
 }
 
-fn fire_player_weapons(
+pub fn fire_player_weapons(
     buttons: Res<Input<MouseButton>>,
     mut events: EventWriter<FireWeaponEvent>,
     weapons: Query<(Entity, &Weapon)>,
@@ -116,20 +103,4 @@ fn fire_player_weapons(
             }
         }
     }
-}
-
-fn camera_movement(
-    cursor: Query<Entity, With<CursorTracker>>,
-    player: Query<Entity, With<Player>>,
-    camera: Res<MainCamera>,
-    mut transforms: Query<&mut Transform2d>,
-) {
-    let player_weight = 0.7;
-    let delay = 0.15;
-    let cursor_loc = transforms.get(cursor.single()).unwrap().translation;
-    let player_loc = transforms.get(player.single()).unwrap().translation;
-    let mut camera_loc = transforms.get_mut(camera.0).unwrap().translation;
-    camera_loc = (cursor_loc * (1. - player_weight) + player_loc * player_weight) * delay
-        + camera_loc * (1. - delay);
-    transforms.get_mut(camera.0).unwrap().translation = camera_loc;
 }
